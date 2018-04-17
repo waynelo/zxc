7cd9118d6eae360102965518db20bdb388285254
Lowe <Lowe@lwh-pc>
2018-04-17 15:01:22 +0800

    完成log4j的配置，并且可以通过slf4j配合log4j完成日志输出

--- src/main/java/cn/lwh/zxc/service/UserService.java
+++ src/main/java/cn/lwh/zxc/service/UserService.java
@@ -3,6 +3,13 @@ package cn.lwh.zxc.service;
 import cn.lwh.zxc.pojo.UserDO;
 
 public interface UserService {
+    
+    /**
+     * 对登录用户进行验证
+     * @Description: TODO 对登录用户进行验证
+     * @param @param user 用户的登录信息
+     * @return UserDO  用户完整信息
+     */
     UserDO login(UserDO user);
     
     UserDO getUser(int id);
--- src/main/java/cn/lwh/zxc/service/impl/UserServiceImpl.java
+++ src/main/java/cn/lwh/zxc/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package cn.lwh.zxc.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +13,21 @@ import cn.lwh.zxc.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
-    
+    private Logger LOGGER = LoggerFactory.getLogger(UserService.class.getName());
     @Autowired
     private UserDao userDao;
     
     public UserDO login(UserDO user) {
         List<UserDO> list = userDao.getUserByUsernameAndPassword(user);
         if(null != list && list.size() > 0) {
-            return list.get(0);
+            UserDO u = list.get(0);
+            LOGGER.info("用户 {} 登录系统" ,u.getUsername());
+            return u;
         }else {
             return null;
         }
     }
+    
     
     public UserDO getUser(int id) {
         List<UserDO> list = userDao.getUserByUserId(id);
